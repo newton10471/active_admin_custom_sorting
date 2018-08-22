7bd5849b3da40d34a4493f3dc8fe104f2d449134
Matt Newton <newton10471@gmail.com>
2018-08-22 11:24:22 -0400

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin2@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin3@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+admin_user_id_1 = AdminUser.first.id
+admin_user_id_2 = AdminUser.second.id
+admin_user_id_3 = AdminUser.third.id
+3.times { GoldStar.create!(admin_user_id: admin_user_id_1) }
+1.times { GoldStar.create!(admin_user_id: admin_user_id_2) }
+2.times { GoldStar.create!(admin_user_id: admin_user_id_3) }
